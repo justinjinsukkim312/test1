7d12185c837577f4e5e100b5d19955bc6e2b24b8
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-07-17 12:15:33 -0500

    get method to api

--- src/app/services/freeapi.service.ts
+++ src/app/services/freeapi.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class freeApiService {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
+  // create an instance of httpclient
+  constructor(private httpclient: HttpClient) {}
+  // return the instance of httpclient
+  getcomments(): Observable<any> {
+    return this.httpclient.get('https://jsonplaceholder.typicode.com/todos');
+  }
 }
